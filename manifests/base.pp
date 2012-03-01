828fb1977adef625d03743241938a6201a81858e
Tim Sharpe <tim@sharpe.id.au>
2012-03-01 16:36:39 +1100

    Initial commit

---
+++ manifests/base.pp
@@ -0,0 +1,25 @@
+class logrotate::base {
+  package { 'logrotate':
+    ensure => latest,
+  }
+
+  File {
+    owner   => 'root',
+    group   => 'root',
+    require => Package['logrotate'],
+  }
+
+  file {
+    '/etc/logrotate.conf':
+      ensure  => file,
+      mode    => '0444',
+      source  => 'puppet:///modules/logrotate/etc/logrotate.conf';
+    '/etc/logrotate.d':
+      ensure  => directory,
+      mode    => '0755';
+    '/etc/cron.daily/logrotate':
+      ensure  => file,
+      mode    => '0555',
+      source  => 'puppet:///modules/logrotate/etc/cron.daily/logrotate';
+  }
+}
