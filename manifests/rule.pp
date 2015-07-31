28bc46789aea6a73883b0ea9f3a9b236e86f8abf
Marcelo Moreira de Mello <mmello@redhat.com>
2015-07-31 18:44:05 -0400

    Removed the require => Class['logrotate::base'] to avoid dependency cycles issues when overriding values on Foreman
     --------------
     Info: Applying configuration version '1438371158'
     Error: Could not apply complete catalog: Found 1 dependency cycle:
     (File[/etc/logrotate.d/messages] => Logrotate::Rule[messages] => Class[Logrotate] => File[/etc/logrotate.d/messages])
     Try the '--graph' option and opening the resulting '.dot' file in OmniGraffle or GraphViz
     Notice: Finished catalog run in 0.08 seconds
     --------------

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -427,6 +427,5 @@ define logrotate::rule(
     group   => 'root',
     mode    => '0444',
     content => template('logrotate/etc/logrotate.d/rule.erb'),
-    require => Class['logrotate::base'],
   }
 }
