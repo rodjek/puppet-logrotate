adae6ae6faf9b35b4abd159608fb074d9576d5eb
Garrett Honeycutt <code@garretthoneycutt.com>
2015-11-06 17:18:19 -0500

    Update testing harness with travis-ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
-require 'rspec-puppet'
+require 'puppetlabs_spec_helper/module_spec_helper'
 
-RSpec.configure do |c|
-  c.module_path = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'modules'))
-  c.manifest_dir = File.expand_path(File.join(__FILE__, '..', 'fixtures', 'manifests'))
+RSpec.configure do |config|
+  config.before :each do
+    Puppet[:parser] = 'future' if ENV['PARSER'] == 'future'
+  end
 end
